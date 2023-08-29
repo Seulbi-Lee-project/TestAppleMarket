41824c1f199e8f938b6a7ec42a40600d713e8200
Seulbi-Lee-project <ifssfws@gmail.com>
2023-08-29 13:18:01 +0900

    feat : view binding

--- app/src/main/java/com/example/testapplemarket/MainActivity.kt
+++ app/src/main/java/com/example/testapplemarket/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.testapplemarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testapplemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
+
     }
 }
