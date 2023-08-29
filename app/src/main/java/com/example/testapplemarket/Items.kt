d1d2fccfe91553dda588a1b27c49044e1c34bf80
Seulbi-Lee-project <ifssfws@gmail.com>
2023-08-29 15:26:15 +0900

    feat : main recyclerview 연결

---
+++ app/src/main/java/com/example/testapplemarket/Items.kt
@@ -0,0 +1,4 @@
+package com.example.testapplemarket
+
+data class Items(var itemImage:Int, var itemSubject:String, var itemContent:String, var itemPlace:String, var itemPrice:Int, var address:String, var commentNumber:Int, var likeNumber:Int) {
+}
---
+++ app/src/main/java/com/example/testapplemarket/ItemsAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.testapplemarket
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testapplemarket.databinding.ItemMainRecyclerviewBinding
+import java.text.DecimalFormat
+
+class ItemsAdapter(var itemsList: MutableList<Items>): RecyclerView.Adapter<ItemsAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemMainRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(itemsList[position])
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemCount(): Int {
+        return  itemsList.size
+    }
+
+    inner class Holder(val binding: ItemMainRecyclerviewBinding): RecyclerView.ViewHolder(binding.root){
+        val itemImage = binding.itemImage
+        val itemSubject = binding.subjectText
+        val itemPlace = binding.placeText
+        val itemPrice = binding.priceText
+        val commentNumber = binding.commentNumber
+        val likeNumber = binding.likeNumber
+
+        fun bind(items:Items){
+            itemImage.setImageResource(items.itemImage)
+            itemSubject.text = items.itemSubject
+            itemPlace.text = items.itemPlace
+            itemPrice.text = DecimalFormat("#,###").format(items.itemPrice) + "원"
+            commentNumber.text = items.commentNumber.toString()
+            likeNumber.text = items.likeNumber.toString()
+        }
+    }
+}
